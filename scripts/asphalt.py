0acff5bb172a7abd8513c5da7bfabcac392bc120
Pavel Svoboda <pavel.svoboda@innovatrics.com>
2018-12-28 22:08:34 +0000

    Add pcards demo [IMAGE-1474]

--- scripts/asphalt.py
+++ scripts/asphalt.py
@@ -21,6 +21,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import argparse
+import os
 import posixpath
 
 import matplotlib.pyplot as plt
@@ -42,40 +43,47 @@ def parse_args():
     parser.add_argument("--ann-h5", required=True, type=str)
     parser.add_argument("--ann-df", required=False, default="df", type=str)
     parser.add_argument("--batch-size", required=False, type=int, default=1)
+    parser.add_argument("--gpu-id", required=False, type=int, default=1)
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
     flags = parse_args()   
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"]="{}".format(flags.gpu_id)
+
     generator = InnH5Asphalt(
-        INPUT_SIZE, flags.img_h5, flags.img_df, flags.ann_h5, flags.ann_df)
+        INPUT_SIZE, flags.img_h5, flags.img_df, flags.ann_h5, flags.ann_df,
+        channels=3)
     
-    fig, ax = plt.subplots(1,2, figsize=(12,4))
-    for _ in range(100):
-        img, label = generator(1)
-        ax[0].imshow(img[0, ..., 0], aspect="auto", cmap=plt.cm.gray)
-        ax[1].imshow(label[0, ..., 1], aspect="auto", cmap=plt.cm.gray)
-        plt.pause(3)
-        plt.draw()
+#    fig, ax = plt.subplots(1,2, figsize=(12,4))
+#    for _ in range(100):
+    img, label = generator(1)
+#        ax[0].imshow(img[0, ..., 0], aspect="auto", cmap=plt.cm.gray)
+#        ax[1].imshow(label[0, ..., 1], aspect="auto", cmap=plt.cm.gray)
+#        plt.pause(3)
+#        plt.draw()
+
+    print(img)
 
     net = unet.Unet(
         channels=generator.channels,
         n_class=generator.n_class,
-        layers=3,
+        layers=5,
         features_root=16,
         summaries=True,
-        cost_kwargs={"class_weights": [0.33, 0.66]})
+        cost_kwargs={"class_weights": [0.3, 0.7]})
 
     trainer = unet.Trainer(
         net,
         optimizer="momentum",
-        batch_size=1,
+        batch_size=flags.batch_size,
         opt_kwargs=dict(learning_rate=0.001))
 
     path = trainer.train(
         generator, "./unet_trained",
         training_iters=10000,
         epochs=10,
-        dropout=0.75,  # probability to keep units
-        display_step=10)
+        dropout=0.8, # probability to keep units
+        display_step=250)
